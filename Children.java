306c1f4164654d0c39d6a9a4134f1d338c44751c
Hemasruthi <a.hemasruthi@gmail.com>
2016-07-21 15:11:44 +0530

    Add files via upload

---
+++ Children.java
@@ -0,0 +1,16 @@
+package children;
+import java.util.Scanner;
+public class Children {
+    public static void main(String[] args) {
+       Scanner sc=new Scanner(System.in);
+       System.out.println("enter n value");
+        int n=sc.nextInt(); int j=1;
+	for(int i=0;i<2*n;i++)	
+        {int x=2*n-i;
+	  if(n!=x) 
+           {
+             System.out.println(" "+j++ +"  "+"  "+x);  
+              }		
+	       }
+    }
+}
